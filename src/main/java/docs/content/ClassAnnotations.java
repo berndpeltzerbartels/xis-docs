f977dc91ec6e065c40b10c72712336a987c308ec
bernd9 <bernd@peltzer-bartels.de>
2025-11-29 15:18:20 +0100

    title variable in templates

--- src/main/java/docs/content/ClassAnnotations.java
+++ src/main/java/docs/content/ClassAnnotations.java
@@ -1,10 +1,16 @@
 package docs.content;
 
 import docs.Navigation;
-import docs.content.templatesyntax.TemplateSyntax;
+import one.xis.ModelData;
 import one.xis.Widget;
 
 @Widget
 @Navigation(title = "Class Annotations", nextItem = MethodAnnotations.class)
 class ClassAnnotations {
+
+    @ModelData
+    String chapterHeadline() {
+        return getClass().getAnnotation(Navigation.class).title();
+    }
+
 }
--- src/main/java/docs/content/MethodAnnotations.java
+++ src/main/java/docs/content/MethodAnnotations.java
@@ -2,9 +2,16 @@ package docs.content;
 
 import docs.Navigation;
 import docs.content.templatesyntax.TemplateSyntax;
+import one.xis.ModelData;
 import one.xis.Widget;
 
 @Widget
 @Navigation(title = "Method Annotations", nextItem = TemplateSyntax.class)
 class MethodAnnotations {
+    
+    @ModelData
+    String chapterHeadline() {
+        return getClass().getAnnotation(Navigation.class).title();
+    }
+
 }
