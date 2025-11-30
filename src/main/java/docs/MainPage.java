537bc554e05be554a98cf9d5a00f5158c8cc68ea
bernd9 <bernd@peltzer-bartels.de>
2025-11-30 22:30:41 +0100

    more content

--- src/main/java/docs/MainPage.java
+++ src/main/java/docs/MainPage.java
@@ -4,18 +4,48 @@ package docs;
 import docs.navi.NaviItemDto;
 import docs.navi.NaviService;
 import lombok.RequiredArgsConstructor;
-import one.xis.ModelData;
-import one.xis.Page;
-import one.xis.WelcomePage;
+import one.xis.*;
+import one.xis.utils.lang.ClassUtils;
+import one.xis.utils.lang.StringUtils;
 
 import java.util.List;
 
 
-@WelcomePage
-@Page("/index.html")
+@Page("/{key}.html")
 @RequiredArgsConstructor
 class MainPage {
     private final NaviService naviService;
+
+    @MethodParameter("widgetClass")
+    Class<?> widgetClass(@PathVariable("key") String key) {
+        if (StringUtils.isEmpty(key) || "index".equals(key)) {
+            return null;
+        }
+        try {
+            return ClassUtils.classForName("docs.content." + key.replace("-", "+"));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @ModelData
+    String defaultWidget(@MethodParameter("widgetClass") Class<?> widgetClass) {
+        if (widgetClass == null) {
+            return null;
+        }
+        return widgetClass.getAnnotation(Widget.class).value().isEmpty() ? widgetClass.getSimpleName() : widgetClass.getAnnotation(Widget.class).value();
+    }
+
+    @Title
+    String pageTitle(@MethodParameter("widgetClass") Class<?> widgetClass) {
+        if (widgetClass == null) {
+            return "Xis1 Documentation";
+        }
+        if (widgetClass.isAnnotationPresent(Navigation.class)) {
+            return widgetClass.getAnnotation(Navigation.class).title();
+        }
+        return "Xis1 Documentation";
+    }
 
     @ModelData
     List<NaviItemDto> naviItems() {
