e268fe19fb867b77583c49b399eebdbe1fe1ffed
bernd9 <bernd@peltzer-bartels.de>
2026-01-04 20:51:20 +0100

    added missing headline

--- src/main/java/xis/quickstart/content/Testing.java
+++ src/main/java/xis/quickstart/content/Testing.java
@@ -1,10 +1,18 @@
 package xis.quickstart.content;
 
 
+import one.xis.ModelData;
+import one.xis.Title;
 import one.xis.Widget;
 import xis.navi.Navigation;
 
 @Widget(id = "quickstart-testing", url = "/quickstart/testing.html", containerId = "main")
 @Navigation(title = "Testing", section = "quickstart", nextItem = RunningTheApp.class)
 class Testing {
+
+    @Title
+    @ModelData
+    String chapterHeadline() {
+        return getClass().getAnnotation(Navigation.class).title();
+    }
 }
